7348f141e6e29ae86775109e045f6defaa2fe906
RobinvanKempen <149374103+robinsrepository@users.noreply.github.com>
2024-11-29 17:29:50 +0100

    Update script.js
    Comment previous code.
    Add new box event.

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-let inputBtn = document.getElementById("input-btn");
+// let inputBtn = document.getElementById("input-btn");
 
-inputBtn.addEventListener("click", function() {
-    console.log("Button clicked on addEventListener");
-});
+// inputBtn.addEventListener("click", function() {
+//     console.log("Button clicked on addEventListener");
+// });
+
+let box = document.getElementById("box");
+
+box.addEventListener("click", function() {
+    console.log("I want to open the box!");
+}); 
