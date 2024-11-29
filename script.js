63ffaf35a8704b69f96684b92d221e34214a89ac
RobinvanKempen <149374103+robinsrepository@users.noreply.github.com>
2024-11-29 21:29:47 +0100

    Update script.js
    Clear out the user input inside of the input element

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let myLeads = [];
-const inputEl = document.getElementById("input-el");
+let inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
-    console.log(myLeads);
+    inputEl.value = "";
     renderLeads();
 });
 
