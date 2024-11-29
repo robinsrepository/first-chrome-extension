8c69eeaf09a8ba7193e5d34975e2f15da65a0ca5
RobinvanKempen <149374103+robinsrepository@users.noreply.github.com>
2024-11-29 21:14:54 +0100

    Improving performance
    By changing up the code a litte: in stead of using .innerHTML 3 times inside of the for loop I use it once outside of the for loop.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+let listItems = "";
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
@@ -10,8 +11,13 @@ inputBtn.addEventListener("click", function() {
 
 // innerHTML method
 for (let i = 0; i < myLeads.length; i++) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+    // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+    listItems += "<li>" + myLeads[i] + "</li>";
 }
+
+ulEl.innerHTML = listItems;
+
+
 
 // // .textContent method
 // for (let i = 0; i < myLeads.length; i++) {
