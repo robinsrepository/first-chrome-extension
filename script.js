72964e4e77d0c21114cc1b90e58fd7848a74de74
RobinvanKempen <149374103+robinsrepository@users.noreply.github.com>
2024-12-01 00:08:06 +0100

    Save key-value pair to localStorage

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
+// localStorage.setItem("myLeads", "www.example.com");
+console.log(localStorage.getItem("myLeads"));
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
     inputEl.value = "";
