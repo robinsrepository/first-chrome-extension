119484d2e287d9911337a7c96b152d55317e82de
RobinvanKempen <149374103+robinsrepository@users.noreply.github.com>
2024-12-02 20:55:36 +0100

    Fetching the myLeads array
    from localStorage

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ let inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+console.log(leadsFromLocalStorage);
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
     inputEl.value = "";
