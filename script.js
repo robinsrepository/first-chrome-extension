4362f7f1d6ce00d46f42f08ee34729d146b12226
RobinvanKempen <149374103+robinsrepository@users.noreply.github.com>
2024-12-03 20:30:27 +0100

    Add if statement
    I learned about truthy and falsy and now added an if statement that checks if there are any leads inside of LocalStorage

--- script.js
+++ script.js
@@ -3,18 +3,23 @@ let inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
+// ["lead1", "lead2"] or null
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 console.log(leadsFromLocalStorage);
 
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    renderLeads();
+}
+
+
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
     inputEl.value = "";
-
     localStorage.setItem("myLeads", JSON.stringify(myLeads));
-
     renderLeads();
-    console.log(localStorage.getItem("myLeads"));
 });
 
 function renderLeads() {
