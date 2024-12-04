785de0673fa623d57bee9795a94e66bc9832b30e
RobinvanKempen <149374103+robinsrepository@users.noreply.github.com>
2024-12-04 16:09:58 +0100

    Refactor the code
    Adding function parameters to improve the code

--- script.js
+++ script.js
@@ -7,7 +7,20 @@ const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage;
-    renderLeads();
+    render(myLeads);
+}
+
+function render(leads) {
+    let listItems = "";
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a href='${leads[i]}' target='_blank'>
+                    ${leads[i]}
+                </a>
+            </li>`;
+     }
+    ulEl.innerHTML = listItems;
 }
 
 console.log(myLeads);
@@ -15,25 +28,13 @@ console.log(myLeads);
 deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear();
     myLeads = [];
-    renderLeads();
+    render(myLeads);
 })
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
     inputEl.value = "";
     localStorage.setItem("myLeads", JSON.stringify(myLeads));
-    renderLeads();
+    render(myLeads);
 });
 
-function renderLeads() {
-    let listItems = "";
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += `
-            <li>
-                <a href='${myLeads[i]}' target='_blank'>
-                    ${myLeads[i]}
-                </a>
-            </li>`;
-     }
-    ulEl.innerHTML = listItems;
-}
