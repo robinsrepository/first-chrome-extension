f37972c2b33089f85e8283bb02b6d458edbb9e17
RobinvanKempen <149374103+robinsrepository@users.noreply.github.com>
2024-12-07 14:48:59 +0100

    Do excercise

--- script.js
+++ script.js
@@ -64,3 +64,9 @@ let data = [
 // Fetch the button from the DOM, store it in a variable
 // Use addEventListener() to listen for button clicks
 // Log Jane's score when the button is clicked (via data)
+
+let scoreBtn = document.getElementById("score-btn");
+
+scoreBtn.addEventListener("click", function() {
+    console.log(data[0].score);
+});
