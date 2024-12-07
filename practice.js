6cb331305c27c1e537b772d630a03795e0491798
RobinvanKempen <149374103+robinsrepository@users.noreply.github.com>
2024-12-07 15:21:47 +0100

    Move excercise code
    To practice.js and past new excercise into script.js

--- practice.js
+++ practice.js
@@ -142,3 +142,24 @@ logCourses(myCourses)
 localStorage.setItem("myCredits", "100");
 let myCredits = localStorage.getItem("myCredits");
 console.log(myCredits);
+
+let data = [
+    {
+        player: "Jane",
+        score: 52
+    }, 
+    {
+        player: "Mark",
+        score: 41
+    }
+]
+
+// Fetch the button from the DOM, store it in a variable
+// Use addEventListener() to listen for button clicks
+// Log Jane's score when the button is clicked (via data)
+
+let scoreBtn = document.getElementById("score-btn");
+
+scoreBtn.addEventListener("click", function() {
+    console.log(data[0].score);
+});
